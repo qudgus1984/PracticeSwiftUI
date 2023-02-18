dc70e6533468148335bc17db1a2e39fb0e696b7a
cody <qudgus1984@naver.com>
2023-02-18 23:36:09 +0900

    Initial Commit

---
+++ SwiftUITodo/SwiftUITodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITodoApp.swift
+//  SwiftUITodo
+//
+//  Created by 이병현 on 2023/02/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
